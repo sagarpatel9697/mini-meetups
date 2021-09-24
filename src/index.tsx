91918d306cb7e7781ade4b0e7686d330d086b226
Sagar Patel <sagarpatel9697@gmail.com>
2021-09-24 21:35:37 +0100

    Hash Router dependency added

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { App } from "./App";
 import "./index.css";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
   document.getElementById("root")
 );
